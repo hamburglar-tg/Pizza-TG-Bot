82b1f1ddd6ef8b0f48ad00d1559a81c7cf4ac93a
Hamburglar <92908058+hamburgIar@users.noreply.github.com>
2024-06-15 00:03:43 +0300

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,11 @@
-bot_token = "7108398191:AAHrH2hvZyKsVkWu_7pdcTPks6pQTlIK9aM"
+bot_token = ""
 
-channel_kb_name = "Точка вкуса"
-channel_tag = "@tochka_vkusa_vlz"
-channel_full_url = "https://t.me/tochka_vkusa_vlz"
+channel_kb_name = ""
+channel_tag = "@"
+channel_full_url = "https://t.me/"
 
-admins = [6546778933]
-bug_manager_id = 6546778933
+admins = []
+bug_manager_id = 
 
 db_name = "database.db"
 
@@ -19,16 +19,16 @@ opening_hours = {
     "Вс": "с 9-00 до 22-00"
 }
 
-latitude = 48.766889
-longitude = 44.799118
+latitude = 
+longitude = 
 
-main_image = "https://imgur.com/e2Mr56z"  # only url
+main_image = "https://imgur.com/"  # only url
 
-phone_number = "+7 (995) 029-44-09"
-address = "ул. Дружбы 77А"
+phone_number = "+7"
+address = "ул."
 
-menu_web_app_url = "https://b574-176-214-113-186.ngrok-free.app"
+menu_web_app_url = "https://ngrok-free.app"
 
-random_prizes = ["Скидка 15% на пиццу Пепперони"]
+random_prizes = []
 
 invite_num = 1
